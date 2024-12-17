e70c331ca47e18e3fa0d5ffa58fb169364f684d4
Anonymous <anonymous@example.com>
2024-12-17 14:28:20 +0800

    add checkpoint link

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ conda activate emailenv
 sudo apt-get install poppler-utils  # For Linux (required by pdf2image)
 sudo apt-get install wkhtmltopdf
 pip install -r requirements.txt
-gdown --id xx -O checkpoints.zip
+gdown --id 10Gu1zlRiCS5ICglNJQGq-2wpPPLKqcZ8 -O checkpoints.zip
 unzip checkpoints.zip -d checkpoints/
 
 
